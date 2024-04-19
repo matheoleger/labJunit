74671743af17e2261247464d5a10a093b6484ba1
matheoleger <matheo.leger@ynov.com>
2024-04-19 11:01:27 +0200

    feat(calc): Add 'somme' and 'produit'

--- src/main/java/labJunit/app/Calculatrice.java
+++ src/main/java/labJunit/app/Calculatrice.java
@@ -5,4 +5,12 @@ public class Calculatrice {
 	public Calculatrice() {
 		System.out.println("Bienvenue");
 	}
+	
+	public double somme(double a, double b) {
+		return a+b;
+	}
+	
+	public double produit(double a, double b) {
+		return a*b;
+	}
 }
--- src/main/java/labJunit/app/Main.java
+++ src/main/java/labJunit/app/Main.java
@@ -2,10 +2,14 @@ package labJunit.app;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+	public static void main(String[] args) {	
 		Calculatrice c = new Calculatrice();
+		
+		double s = c.somme(10, 20);
+		System.out.println("La somme de 10 et 20 est " + s);
+		
+		double p = c.produit(10, 20);
+		System.out.println("Le produit de 10 et 20 est " + p);
 	}
 
 }
