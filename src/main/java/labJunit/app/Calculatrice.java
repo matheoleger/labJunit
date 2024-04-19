596f4b3cedfafd3bfa7db7e690ac76f4bd7242ac
matheoleger <matheo.leger@ynov.com>
2024-04-19 10:53:18 +0200

    setup maven project

---
+++ src/main/java/labJunit/app/Calculatrice.java
@@ -0,0 +1,8 @@
+package labJunit.app;
+
+public class Calculatrice {
+
+	public Calculatrice() {
+		System.out.println("Bienvenue");
+	}
+}
---
+++ src/main/java/labJunit/app/Main.java
@@ -0,0 +1,11 @@
+package labJunit.app;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Calculatrice c = new Calculatrice();
+	}
+
+}
