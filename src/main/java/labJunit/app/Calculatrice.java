435be939a7810e29ba683fb143dc097aa0da0f94
matheoleger <matheo.leger@ynov.com>
2024-04-19 11:48:38 +0200

    feat(mvn): Add manifest plugin and deps

--- src/main/java/labJunit/app/Calculatrice.java
+++ src/main/java/labJunit/app/Calculatrice.java
@@ -3,7 +3,7 @@ package labJunit.app;
 public class Calculatrice {
 
 	public Calculatrice() {
-		System.out.println("Bienvenue");
+		//System.out.println("Bienvenue");
 	}
 	
 	public double somme(double a, double b) {
