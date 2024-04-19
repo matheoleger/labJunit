c0d557caf40bc230b46750027e835cb7a7bbea68
matheoleger <matheo.leger@ynov.com>
2024-04-19 16:28:47 +0200

    feat(main): Change println

--- src/main/java/labJunit/app/Main.java
+++ src/main/java/labJunit/app/Main.java
@@ -11,7 +11,7 @@ public class Main {
 		double p = c.produit(10, 20);
 		System.out.println("Le produit de 10 et 20 est " + p);
 		
-		System.out.println("Test the webhook!");
+		System.out.println("Test!");
 	}
 
 }
